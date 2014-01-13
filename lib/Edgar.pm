31ab2add0508ab5ff3aa46ccc8fb81d25d5d60fa
Vikas N Kumar <vikas@cpan.org>
2014-01-13 11:38:33 -0500

    Starting the perl module

---
+++ lib/Edgar.pm
@@ -0,0 +1,40 @@
+package Finance::SEC::Edgar;
+use strict;
+use warnings;
+use Carp;
+use LWP;
+
+BEGIN {
+    our $VERSION = '0.01';
+    $VERSION = eval $VERSION;
+}
+
+1;
+
+__END__
+#### COPYRIGHT: 2014. Vikas N Kumar. All Rights Reserved
+#### AUTHOR: Vikas N Kumar <vikas@cpan.org>
+#### DATE: 13th Jan 2014
+#### LICENSE: Refer LICENSE file.
+
+=head1 NAME
+
+Finance::SEC::Edgar
+
+=head1 SYNOPSIS
+
+Finance::SEC::Edgar is used to download and parse the SEC EDGAR filings.
+
+=head1 AUTHORS
+
+Vikas N Kumar <vikas@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright: 2014, Vikas N Kumar <vikas@cpan.org>. All Rights Reserved.
+
+=head1 LICENSE
+
+This is free software. You can redistribute it or modify it under the terms of
+Perl itself. Refer LICENSE file in the top level source directory for more
+information.
---
+++ t/00_use.t
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use Test::More;
+
+BEGIN { use_ok('Finance::SEC::Edgar'); }
+
+done_testing();
+
+__END__
+#### COPYRIGHT: 2014. Vikas N Kumar. All Rights Reserved
+#### AUTHOR: Vikas N Kumar <vikas@cpan.org>
+#### DATE: 13th Jan 2014
+#### LICENSE: Refer LICENSE file.
+
